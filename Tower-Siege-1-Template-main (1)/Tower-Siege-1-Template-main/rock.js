d8ecb488f3e033cbbc0a6b5a2ec9beef1feeda13
Harinandan661 <81477479+Harinandan661@users.noreply.github.com>
2021-07-19 21:16:08 +0530

    Add files via upload

---
+++ Tower-Siege-1-Template-main (1)/Tower-Siege-1-Template-main/rock.js
@@ -0,0 +1,17 @@
+class Rock extends Block {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("polygon.png");
+      
+    }
+  
+    display() {
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+  
+      super.display();
+  
+     
+    }
+  }
+  
